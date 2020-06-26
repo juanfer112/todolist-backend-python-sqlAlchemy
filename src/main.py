7d9504836d86d755d60b32b2f46d10f9a8a67ca5
Juan Pollan <juanferformacionyproyectos@gmail.com>
2020-06-26 15:26:56 +0000

    desarrollo api todo list fase prueba

--- src/main.py
+++ src/main.py
@@ -2,19 +2,23 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
+from sqlalchemy import Column, ForeignKey, Integer, String
+from flask import Flask, request, jsonify, url_for, json
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from models import db, Todo
+from models import db,Todos
+
 
 app = Flask(__name__)
+
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db)
 db.init_app(app)
+
 CORS(app)
 
 # Handle/serialize errors like a JSON object
@@ -27,25 +31,35 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-
 @app.route('/todos', methods=['GET'])
-
-def get_todos():
-    body=jsonify(todos)
-    return body
+def get_task():
+    todos=Todos.query.all()
+    todos=list(map(lambda task: task.serialize(),todos))
+    return jsonify(todos),200
 
 @app.route('/todos', methods=['POST'])
+def post_task():
+    request_body = request.get_json() # get the request body content
 
-def add_new_todo():
-    request_body= request.get_json()
-    label = request.json['label'] 
-    done = request.json['done'] 
-    new_task = Todo(label,done)
-    db.session.add(new_task)
-    # decoded_object=json.loads(request_body)
-    print("Incoming request with the following body", request_body)
-    return request_body
+    # if 'done' not in body:
+    #     return 'please specify true or false in done',400
+    # if 'label' not in body:
+    #     return 'please specify the label', 400
+    task=Todos(done=request_body['done'],label=request_body['label'])
+    db.session.add(task)
+    db.session.commit()
+    return jsonify(task.serialize()),200
 
+@app.route('/todos/<id>', methods=['DELETE'])
+def delete_task():
+    deleted_todo=Todos.query.filter_by(id)
+    db.session.delete(deleted_todo)
+    db.session.commit()
+    return jsonify(deleted_todo.serialize()),200
+
+
+# this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000, debug=True)
+    PORT = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=PORT, debug=True)
 
--- src/models.py
+++ src/models.py
@@ -2,21 +2,16 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-
-engine = create_engine('mysql://root@localhost:3306/example')
-
-class Todo(db.Model):
-    __tablename__ = 'todolist'
+class Todos(db.Model):
+    __tablename__='Todos'
     id = db.Column(db.Integer, primary_key=True)
-    label = db.Column(db.String(250), unique=True, nullable=False)
-    done = db.Column(db.Boolean, default=False, nullable=False)
-
-    def __repr__(self):
-        return '<Todo %r>' % self.label
+    done = db.Column(db.Boolean, unique=True, nullable=False)
+    label = db.Column(db.String(255), unique=True, nullable=False)
+    
 
     def serialize(self):
         return {
-            "id": self.id,
-            "label": self.label,
-            "done": self.done
+            "id":self.id,
+            "done": self.done,
+            "label": self.label
         }
